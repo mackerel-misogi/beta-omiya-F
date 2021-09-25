bb0f80a4f25f41349b1e66d94cd5e8a106af4d81
鯖の禊 <80382375+mackerel-misogi@users.noreply.github.com>
2021-09-25 13:01:00 +0900

    Update hint.js

--- assets/js/hint.js
+++ assets/js/hint.js
@@ -7,3 +7,32 @@ function show2(){
   var hinttext2 = document.getElementById("hint-2");
   hinttext2.style.display = "inline-block";
 }
+function show3(){
+  var hinttext3 = document.getElementById("hint-3");
+  hinttext3.style.display = "inline-block";
+}
+
+function show4(){
+  var hinttext4 = document.getElementById("hint-4");
+  hinttext4.style.display = "inline-block";
+}
+
+function show5(){
+  var hinttext5 = document.getElementById("hint-5");
+  hinttext5.style.display = "inline-block";
+}
+
+function show6(){
+  var hinttext6 = document.getElementById("hint-6");
+  hinttext6.style.display = "inline-block";
+}
+
+function show7(){
+  var hinttext7 = document.getElementById("hint-7");
+  hinttext7.style.display = "inline-block";
+}
+
+function show8(){
+  var hinttext8 = document.getElementById("hint-8");
+  hinttext8.style.display = "inline-block";
+}
